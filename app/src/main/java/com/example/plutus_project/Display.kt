2cafb9eba977ae6e950a1a577f5ddbe956e584cd
LI YAN <yan.li@localhost.localdomain>
2022-05-11 10:43:35 +0200

    add currency menu

--- app/src/main/java/com/example/plutus_project/Display.kt
+++ app/src/main/java/com/example/plutus_project/Display.kt
@@ -1,7 +1,10 @@
 package com.example.plutus_project
 
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.*
@@ -17,6 +20,35 @@ fun DrawTransaction(){
                 TextField(value = amount, onValueChange = {amount = it},
                     placeholder = { Text(text = "Montant")}
                 )
+                var expanded by remember{ mutableStateOf(false)}
+                val items = listOf("A", "B", "C", "D", "E", "F")
+                //val disabledValue = "B"
+                var selectedIndex by remember { mutableStateOf(0) }
+                Box(modifier = Modifier.fillMaxSize()) {
+                    Text(items[selectedIndex],modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable(onClick = { expanded = true }))
+                    DropdownMenu(
+                        expanded = expanded,
+                        onDismissRequest = { expanded = false },
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        items.forEachIndexed { index, s ->
+                            DropdownMenuItem(onClick = {
+                                selectedIndex = index
+                                expanded = false
+                            }) {
+                                //val disabledText = if (s == disabledValue) {
+                                //    " (Disabled)"
+                                //} else {
+                                //   ""
+                                //}
+                                //Text(text = s + disabledText)
+                                Text(text = s)
+                            }
+                        }
+                    }
+                }
             }
             
         }
